88cbdf1eba588019f9262b6ef603ece2ee4dc3de
Spacial <spacial@p4r00t>
2025-02-21 18:53:02 -0300

    shellcode, finder, victm and guesser - page 32 - part 1

---
+++ bof/0x07_find_start.c
@@ -0,0 +1,10 @@
+// find_start.c
+#include <stdlib.h>
+#include <stdio.h>
+unsigned long find_start(void) {
+ __asm__("movl %esp, %eax");
+}
+int main() {
+ printf("0x%x\n",find_start());
+}
+
---
+++ bof/0x08_victim.c
@@ -0,0 +1,13 @@
+// victim.c
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc,char *argv[]){
+		char little_array[512];
+	if (argc > 1)
+		strcpy(little_array,argv[1]);
+}
+
+
+
